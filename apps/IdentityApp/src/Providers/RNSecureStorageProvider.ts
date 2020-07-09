8fbe1a46acaf36adf78661ba84eaa4a16749ac5b
Jesse Clark <hello@developerjesse.com>
2020-07-09 13:45:15 +0300

    Code Review Changes
    - Remove package.lock
    - Update prettier settings
    - RN Secure Storage Provider - return promise

--- apps/IdentityApp/src/Providers/RNSecureStorageProvider.ts
+++ apps/IdentityApp/src/Providers/RNSecureStorageProvider.ts
@@ -1,33 +1,20 @@
 /**
  * Provider for RN-Secure-Storage
  */
-import RNSecureStorage, {ACCESSIBLE} from 'rn-secure-storage';
+import RNSecureStorage, { ACCESSIBLE } from 'rn-secure-storage';
 
 const set = async (key: string, value: string) => {
-  await RNSecureStorage.set(key, value, {
+  return await RNSecureStorage.set(key, value, {
     accessible: ACCESSIBLE.WHEN_UNLOCKED,
-  }).then(
-    (res: any) => {
-      console.log('value set', res);
-      return value;
-    },
-    (err: any) => {
-      console.log(err);
-    },
-  );
+  });
 };
 
 const remove = async (key: string) => {
-  await RNSecureStorage.remove(key)
-  .then((val) => {
-    return val;
-  }).catch((err) => {
-    return err.message;
-  });
-}
+  return await RNSecureStorage.remove(key);
+};
 
 const get = async (key: string) => {
   return await RNSecureStorage.get(key);
 };
 
-export default {get, set, remove};
+export default { get, set, remove };

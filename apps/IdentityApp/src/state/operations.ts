386af7e8c349511334bc39d172d6abceb383eacb
Jesse Clark <hello@developerjesse.com>
2020-07-09 20:09:36 +0300

    Let Loading be the initial screen and navigate with initial startup screen.

--- apps/IdentityApp/src/state/operations.ts
+++ apps/IdentityApp/src/state/operations.ts
@@ -1,5 +1,6 @@
 import { Dispatch } from 'react';
 import { changeLanguage } from 'redux-multilanguage';
+import * as RootNavigation from '../AppNavigation';
 import { StorageProvider, STORAGE_KEYS } from '../Providers/index';
 import { requestIsSignedUp, receiveIsSignedUp } from './localUi/actions';
 
@@ -7,12 +8,14 @@ export const initialAppStart = () => async (dispatch: Dispatch) => {
   dispatch(requestIsSignedUp());
   await StorageProvider.get(STORAGE_KEYS.PIN)
     .then(res => {
-      console.log('PIN response', res);
+      console.log('CREDENTIALS!', res);
       dispatch(receiveIsSignedUp(true));
+      RootNavigation.navigate('CredentialsFlow', { screen: 'SigninWithPin' });
     })
     .catch(() => {
-      console.log('PIN not set!');
+      console.log('WELCOME!');
       dispatch(receiveIsSignedUp(false));
+      RootNavigation.navigate('SignupFlow', { screen: 'Welcome' });
     });
 
   await StorageProvider.get('LANGUAGE')

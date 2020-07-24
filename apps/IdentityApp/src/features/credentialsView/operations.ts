b14c3354b739485e52d0daf38a702ad24f7c4e63
Jesse Clark <hello@developerjesse.com>
2020-07-24 16:51:45 +0300

    Get Buffer working with React Native
    - Install RN-nodeify which creates a ./shim.js file that allows access to Buffer
    - "postinstall" installs buffer and process, and creates the shim.js file
    - ethr-did package relies on buffer, and can now create a presentation with hard-coded data

--- apps/IdentityApp/src/features/credentialsView/operations.ts
+++ apps/IdentityApp/src/features/credentialsView/operations.ts
@@ -2,13 +2,15 @@ import { Dispatch } from 'redux';
 import axios from 'axios';
 import { keccak256 } from 'js-sha3';
 import EthrDID from 'ethr-did';
-import { Buffer } from 'buffer';
-// global.Buffer = Buffer; // very important
-global.Buffer = global.Buffer || require('buffer').Buffer
 
-import { JwtPresentationPayload, createVerifiablePresentationJwt } from 'did-jwt-vc'
+import { JwtPresentationPayload, createVerifiablePresentationJwt } from 'did-jwt-vc';
 import { Credential, CredentialStatus } from './reducer';
-import { requestAllPendingStatus, receiveAllPendingStatus, requestPresentation, receivePresentation } from './actions';
+import {
+  requestAllPendingStatus,
+  receiveAllPendingStatus,
+  requestPresentation,
+  receivePresentation,
+} from './actions';
 import { StorageProvider, STORAGE_KEYS, ISSUER_SERVER } from '../../Providers';
 import {
   requestCredential,
@@ -171,26 +173,31 @@ export const checkStatusOfCredentials = (
   dispatch(receiveAllPendingStatus(resultArray));
 };
 
-
 export const createPresentation = (didHash: string) => async (dispatch: Dispatch) => {
   console.log('getting presentation!', didHash);
+
+  const jwt =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSJdLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIl0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7InR5cGUiOiJJRCIsImZ1bGxOYW1lIjoiSmVzc2UgQ2xhcmsiLCJwaG9uZSI6IjMwMzgwNzU5ODEiLCJiaXJ0aGRhdGUiOiIyMDIwLTA3LTAzIn19LCJzdWIiOiJkaWQ6ZXRocjpyc2s6MHgxMjM0NTY3ODAxMDEwMTAxMDEwMTAxMDAxIiwibmJmIjoxNTYyOTUwMjgyLCJpc3MiOiJkaWQ6ZXRocjoweDQ2QjlGRmQ1QzViREZiNTgwMEYxYmRmMWRlRDk4NDYzQUZiMEI2NmUifQ.rrNhLYAFI-Hl-EjDLeUOKsBOcEATaCWZhutYwibSKDnLY8DVKk--ffcY-i5NpgxfD6R49NWScw6pvV-BLCAsSQ';
   dispatch(requestPresentation());
-  /*
+
   const vpPayload: JwtPresentationPayload = {
     vp: {
       '@context': ['https://www.w3.org/2018/credentials/v1'],
       type: ['VerifiablePresentation'],
-      verifiableCredential: [jwt]
-    }
-  }
+      verifiableCredential: [jwt],
+    },
+  };
 
   const holder = new EthrDID({
     address: '0x46B9FFd5C5bDFb5800F1bdf1deD98463AFb0B66e',
-    privateKey: '0x0',
-  })
+    privateKey: '0x408f89abeca08043e74c01f951f428a4fd5206b10c383648171dc9d86812d2d8',
+  });
 
-  createVerifiablePresentationJwt(vpPayload, holder)
-    .then(presentation => dispatch(receivePresentation(presentation)));
-    */
-  dispatch(receivePresentation('hello'));
+  createVerifiablePresentationJwt(vpPayload, holder).then(presentation => {
+    console.log('PRESENTATION!');
+    console.log(presentation);
+    dispatch(receivePresentation(presentation));
+  });
+
+  // dispatch(receivePresentation('hello'));
 };

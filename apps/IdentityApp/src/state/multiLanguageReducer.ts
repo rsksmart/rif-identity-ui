509e64db56337a1381fab4aa085e3f79155b30a5
Jesse Clark <hello@developerjesse.com>
2020-07-27 18:30:53 +0300

    Create new Languages package
    - move JSON files and export inital state
    - install babel

--- apps/IdentityApp/src/state/multiLanguageReducer.ts
+++ apps/IdentityApp/src/state/multiLanguageReducer.ts
@@ -1,11 +1,4 @@
 import { createMultilanguageReducer } from 'redux-multilanguage';
-import en from '../Languages/en.json';
-import es from '../Languages/es.json';
+import initialState from '@rsksmart/languages';
 
-const currentLanguageCode = 'en';
-export const languages = {
-  en,
-  es,
-};
-
-export default createMultilanguageReducer({ currentLanguageCode, languages });
+export default createMultilanguageReducer(initialState);
---
+++ packages/Languages/index.js
@@ -0,0 +1,11 @@
+import en from './en.json';
+import es from './es.json';
+
+export const currentLanguageCode = 'en';
+
+export const languages = {
+  en,
+  es,
+};
+
+export default { currentLanguageCode, languages }

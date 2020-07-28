9a4aafc2171ee2f24881aa655466f2044f06b259
Jesse Clark <hello@developerjesse.com>
2020-07-28 16:24:10 +0300

    Move create multilanguage reducer to the store.ts file.

--- apps/IdentityApp/src/state/multiLanguageReducer.ts
+++
@@ -1,4 +0,0 @@
-import { createMultilanguageReducer } from 'redux-multilanguage';
-import initialState from '@rsksmart/languages';
-
-export default createMultilanguageReducer(initialState);
--- apps/IdentityApp/src/state/store.ts
+++ apps/IdentityApp/src/state/store.ts
@@ -1,12 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createMultilanguageReducer } from 'redux-multilanguage';
 import thunk from 'redux-thunk';
+import initialLanguageState from '@rsksmart/languages';
 // import {createLogger} from 'redux-logger';
 import customLogger from './middleware/customLogger';
 
 import localUiReducer from './localUi/reducer';
 import signupReducer from '../screens/signup/reducer';
 import profileReducer from '../features/profile/reducer';
-import multiLanguageReducer from './multiLanguageReducer';
 import credentialReducer from '../features/credentialsView/reducer';
 
 const middleware = [thunk, customLogger];
@@ -16,7 +17,7 @@ const rootReducer = combineReducers({
   signup: signupReducer,
   profile: profileReducer,
   credentials: credentialReducer,
-  multilanguage: multiLanguageReducer,
+  multilanguage: createMultilanguageReducer(initialLanguageState),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;

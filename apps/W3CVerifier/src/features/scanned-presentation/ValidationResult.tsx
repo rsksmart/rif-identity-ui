c69daa7028cc031b3212db9290430a433f21f5cc
Jesse Clark <hello@developerjesse.com>
2020-08-11 13:49:08 +0300

    Add credential type icon to validation result component.

--- apps/W3CVerifier/src/features/scanned-presentation/ValidationResult.tsx
+++ apps/W3CVerifier/src/features/scanned-presentation/ValidationResult.tsx
@@ -7,6 +7,7 @@ import {
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
 import { colors } from '../../styles';
 import { VerifiedPresentation } from '../../api';
 
